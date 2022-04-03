4a9cc1be258dc764b0649774f0380903bb3f0eb9
Rossita <47862499+AaronsonRossita@users.noreply.github.com>
2022-04-03 21:30:06 +0300

    Initial commit

---
+++ src/com/company/Animal.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Animal {
+
+// 1. fields
+    boolean hasTail; // true or false
+    String name;
+    int age;
+
+// 2. constructor
+
+    public Animal(boolean hasTail, String name, int age) {
+        this.hasTail = hasTail;
+
+        if (name.isEmpty()){ // ""
+            this.name = "noname";
+        }else{
+            this.name = name;
+        }
+
+        if (age == 0){
+            this.age = 1;
+        }else{
+            this.age = age;
+        }
+    }
+
+// 3. methods (functions)
+
+    public void makeSound(){
+        System.out.println("animal makes sound");
+    }
+
+    public void stam(){
+        System.out.println("stam");
+    }
+
+    @Override
+    public String toString() {
+        return "This animal's name is " + name;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Object object1 = new Object();
+
+        Animal animal1 = new Animal(false,"",0);
+        Cat cat1 = new Cat("Zina",8,9);
+        Dog dog1 = new Dog("Tair",12,true);
+
+//        System.out.println(animal1);
+//        System.out.println(cat1);
+//        System.out.println(dog1);
+//        System.out.println(animal1.toString());
+//        System.out.println(animal1);
+//
+//        System.out.println(cat1);
+//        animal1.makeSound();
+//        cat1.makeSound();
+//        dog1.makeSound();
+//
+//                    0  1  2  3  4
+
+
+    }
+
+}
